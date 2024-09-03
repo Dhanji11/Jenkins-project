16cdb90b93dc3f1b08e66cae31b0dc529feca76a
Dhanji Verma <97548521+Dhanji11@users.noreply.github.com>
2024-09-03 15:49:41 +0530

    resolved error

--- Main.java
+++ Main.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import java.util.*;
 class Main {
     public static void main(String[] args) {
@@ -7,12 +6,4 @@ class Main {
         System.out.println("Successfull");
         System.out.println(new Date());
     }
-=======
-class Main {
-    public static void main(String[] args) {
-        System.out.println("This is Demo Project..");
-        System.out.println("Deploy using jenkins");
-        System.out.println("Successfull");
-    }
->>>>>>> 3d6f789a1baf1a63b76a51530c7c56cd29e98815
 }
