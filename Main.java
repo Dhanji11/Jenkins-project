3d6f789a1baf1a63b76a51530c7c56cd29e98815
Dhanji Verma <97548521+Dhanji11@users.noreply.github.com>
2024-09-03 14:28:46 +0530

    Add files via upload
    this is first commit.

---
+++ Main.java
@@ -0,0 +1,7 @@
+class Main {
+    public static void main(String[] args) {
+        System.out.println("This is Demo Project..");
+        System.out.println("Deploy using jenkins");
+        System.out.println("Successfull");
+    }
+}
