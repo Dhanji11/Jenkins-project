a8b7abb9d057149f5f5fc558adaaadbcd416ada8
Dhanji Verma <97548521+Dhanji11@users.noreply.github.com>
2024-09-03 15:39:26 +0530

    this is new commit after error solve

---
+++ Main.java
@@ -0,0 +1,9 @@
+import java.util.*;
+class Main {
+    public static void main(String[] args) {
+        System.out.println("This is Demo Project..");
+        System.out.println("Deploy using jenkins");
+        System.out.println("Successfull");
+        System.out.println(new Date());
+    }
+}
